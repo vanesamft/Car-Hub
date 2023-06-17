1b0605a8130d979bb8c037c2c9cc817d2befb31b
ituitis-myftari19 <>
2023-06-17 15:51:50 +0300

    initial commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,34 @@
+"use client";
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import CustomButton from "./CustomButton";
+
+const Navbar = () => {
+  return (
+    <header className="w-full absolute z-10">
+      <nav className="max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4">
+        <Link href="/" className="flex justify-center items-center">
+          <Image
+            src="logo.svg"
+            alt="Car Hub Logo"
+            width={118}
+            height={18}
+            className="object-contain"
+          />
+        </Link>
+
+        <CustomButton
+          title="GitHub"
+          btnType="button"
+          containerStyles="text-primary-blue rounded-full bg-white min-w-[130px]"
+          handleClick={() =>
+            window.open("https://github.com/vanesamft/Car-Hub", "_blank")
+          }
+        />
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
